ad8a2287ed83f7a7384bfb9dd439a0cc9770ef22
Adam Cox (Vorba Corp) <adam.cox@vorba.com>
2015-01-20 10:24:05 -0500

    first commit

---
+++ pureVision/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pureVision.Startup))]
+namespace pureVision
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
